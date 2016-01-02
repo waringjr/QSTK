6f027c11bc621b399ac0c38c01717e6f8cf13541
waringjr <waringjr@gmail.com>
2016-01-02 13:53:33 -0500

    initial add of homework1

---
+++ Wk3/hw1.py
@@ -0,0 +1,14 @@
+Python 2.7.3 (default, Apr 10 2012, 23:31:26) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+#Homework 1; Computational Investing; Coursera 2 Jan 2015
+ 
+
+# QSTK Imports
+import QSTK.qstkutil.qsdateutil as du
+import QSTK.qstkutil.tsutil as tsu
+import QSTK.qstkutil.DataAccess as da
+
+# Third Party Imports
+import datetime as dt
+import matplotlib.pyplot as plt
+import pandas as pd
